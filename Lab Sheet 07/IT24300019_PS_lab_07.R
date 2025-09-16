161f09db3cf10b6cc95ef9577bc5d76613388d49
it24300019 <it24300019@my.sliit.lk>
2025-09-16 22:41:32 +0530

    IT24300019_PS_Exercise

---
+++ Lab Sheet 07/IT24300019_PS_lab_07.R
@@ -0,0 +1,26 @@
+getwd()
+setwd("C://Users//Havindu Sineth//Desktop//IT24300019_PS Lab-07")
+
+punif(10,min = 0 ,max = 30 ,lower.tail = TRUE)
+1 - punif(20,min = 0 ,max = 30 ,lower.tail = TRUE)
+punif(10,min = 0 ,max = 30 ,lower.tail = FALSE)
+
+pexp(3,rate = 0.5,lower.tail = TRUE)
+1-pexp(4,rate = 0.5,lower.tail = TRUE)
+pexp(3,rate = 0.5,lower.tail = FALSE)
+
+pexp(4,rate = 0.5,lower.tail = TRUE)-pexp(2,rate = 0.5,lower.tail = TRUE)
+
+1- pnorm(37.9,mean = 36.8,sd =0.4 ,lower.tail = TRUE)
+
+ pnorm(37.9,mean = 36.8,sd =0.4 ,lower.tail = TRUE)-pnorm(36.4,mean = 36.8,sd =0.4 ,lower.tail = TRUE)
+ 
+qnorm(0.012,mean = 36.8 ,sd =0.4 , lower.tail = TRUE)
+qnorm(0.01,mean = 36.8 ,sd =0.4 , lower.tail = FALSE)
+
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+pexp(2, rate = 1/3)
+1 - pnorm(130, mean = 100, sd = 15)
+qnorm(0.95, mean = 100, sd = 15)
+
+
